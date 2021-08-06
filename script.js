1de59bc99992b674747b410dbf272383c7572714
MiKhai37 <mi.khai.thai@gmail.com>
2021-08-07 01:28:41 +0200

    Add black filling feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let gridSize = 16;
+let gridSize = 64;
 
 const gridContainer = document.querySelector('#grid-container');
 
@@ -8,16 +8,25 @@ gridContainer.style.gridTemplateColumns = "repeat(" + gridSize + ", 30px)";
 let divArr = [[]];
 
 for (let i = 1; i <= gridSize; i++) {
-    console.log(i)
     divArr[i] = document.createElement('div')
     for (let j = 1; j <= gridSize; j++) {
         divArr[i][j] = document.createElement('div');
-        divArr[i][j].classList.add('row-' + i);
-        divArr[i][j].classList.add('col-' + j);
+        divArr[i][j].classList.add('square')
+        //divArr[i][j].classList.add('row-' + i);
+        //divArr[i][j].classList.add('col-' + j);
         divArr[i][j].style.gridRowStart = i;
         divArr[i][j].style.gridColumnStart = j;
         //divArr[i][j].textContent = i + " " + j;
         gridContainer.appendChild(divArr[i][j]);
     }
 }
+
+function colorBlack() {
+    this.classList.add('blackFill')
+}
+
+
+const squares = document.querySelectorAll('.square')
+squares.forEach(square => square.addEventListener('mouseenter', colorBlack))
+//squares.forEach(square => square.addEventListener('mouseleave', colorBlack))
 
